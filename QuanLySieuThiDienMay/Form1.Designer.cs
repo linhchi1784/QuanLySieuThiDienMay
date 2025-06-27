ef5568bcaaeb330f62529f48369cd0578f2bae64
Asus <Asus@LAPTOP-8T8DCA59>
2025-06-27 08:55:27 +0700

    Add project files.

---
+++ QuanLySieuThiDienMay/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace QuanLySieuThiDienMay
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            menuStrip1 = new MenuStrip();
+            tsmMenu = new ToolStripMenuItem();
+            thôngTinNhânViênToolStripMenuItem = new ToolStripMenuItem();
+            quảnLýChấmCôngToolStripMenuItem1 = new ToolStripMenuItem();
+            quảnLýHóaĐơnToolStripMenuItem = new ToolStripMenuItem();
+            lậpHóaĐơnToolStripMenuItem = new ToolStripMenuItem();
+            thôngTinHóaĐơnToolStripMenuItem = new ToolStripMenuItem();
+            quảnLýKhoToolStripMenuItem = new ToolStripMenuItem();
+            thôngTinKoToolStripMenuItem = new ToolStripMenuItem();
+            thôngYinNhàToolStripMenuItem = new ToolStripMenuItem();
+            thốngKêVàBáoCáoToolStripMenuItem = new ToolStripMenuItem();
+            thốngKêDoanhThuToolStripMenuItem = new ToolStripMenuItem();
+            tínhLươngNhânViênToolStripMenuItem = new ToolStripMenuItem();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.ImageScalingSize = new Size(20, 20);
+            menuStrip1.Items.AddRange(new ToolStripItem[] { tsmMenu, quảnLýHóaĐơnToolStripMenuItem, quảnLýKhoToolStripMenuItem, thốngKêVàBáoCáoToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(831, 28);
+            menuStrip1.TabIndex = 0;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // tsmMenu
+            // 
+            tsmMenu.DropDownItems.AddRange(new ToolStripItem[] { thôngTinNhânViênToolStripMenuItem, quảnLýChấmCôngToolStripMenuItem1 });
+            tsmMenu.Name = "tsmMenu";
+            tsmMenu.Size = new Size(140, 24);
+            tsmMenu.Text = "Quản lý nhân viên";
+            // 
+            // thôngTinNhânViênToolStripMenuItem
+            // 
+            thôngTinNhânViênToolStripMenuItem.Name = "thôngTinNhânViênToolStripMenuItem";
+            thôngTinNhânViênToolStripMenuItem.Size = new Size(222, 26);
+            thôngTinNhânViênToolStripMenuItem.Text = "Thông tin nhân viên";
+            // 
+            // quảnLýChấmCôngToolStripMenuItem1
+            // 
+            quảnLýChấmCôngToolStripMenuItem1.Name = "quảnLýChấmCôngToolStripMenuItem1";
+            quảnLýChấmCôngToolStripMenuItem1.Size = new Size(222, 26);
+            quảnLýChấmCôngToolStripMenuItem1.Text = "Quản lý chấm công";
+            // 
+            // quảnLýHóaĐơnToolStripMenuItem
+            // 
+            quảnLýHóaĐơnToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { lậpHóaĐơnToolStripMenuItem, thôngTinHóaĐơnToolStripMenuItem });
+            quảnLýHóaĐơnToolStripMenuItem.Name = "quảnLýHóaĐơnToolStripMenuItem";
+            quảnLýHóaĐơnToolStripMenuItem.Size = new Size(132, 24);
+            quảnLýHóaĐơnToolStripMenuItem.Text = "Quản lý hóa đơn";
+            // 
+            // lậpHóaĐơnToolStripMenuItem
+            // 
+            lậpHóaĐơnToolStripMenuItem.Name = "lậpHóaĐơnToolStripMenuItem";
+            lậpHóaĐơnToolStripMenuItem.Size = new Size(214, 26);
+            lậpHóaĐơnToolStripMenuItem.Text = "Lập hóa đơn";
+            // 
+            // thôngTinHóaĐơnToolStripMenuItem
+            // 
+            thôngTinHóaĐơnToolStripMenuItem.Name = "thôngTinHóaĐơnToolStripMenuItem";
+            thôngTinHóaĐơnToolStripMenuItem.Size = new Size(214, 26);
+            thôngTinHóaĐơnToolStripMenuItem.Text = "Thông tin hóa đơn";
+            // 
+            // quảnLýKhoToolStripMenuItem
+            // 
+            quảnLýKhoToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { thôngTinKoToolStripMenuItem, thôngYinNhàToolStripMenuItem });
+            quảnLýKhoToolStripMenuItem.Name = "quảnLýKhoToolStripMenuItem";
+            quảnLýKhoToolStripMenuItem.Size = new Size(101, 24);
+            quảnLýKhoToolStripMenuItem.Text = "Quản lý kho";
+            // 
+            // thôngTinKoToolStripMenuItem
+            // 
+            thôngTinKoToolStripMenuItem.Name = "thôngTinKoToolStripMenuItem";
+            thôngTinKoToolStripMenuItem.Size = new Size(247, 26);
+            thôngTinKoToolStripMenuItem.Text = "Thông tin kho";
+            // 
+            // thôngYinNhàToolStripMenuItem
+            // 
+            thôngYinNhàToolStripMenuItem.Name = "thôngYinNhàToolStripMenuItem";
+            thôngYinNhàToolStripMenuItem.Size = new Size(247, 26);
+            thôngYinNhàToolStripMenuItem.Text = "Thông tin nhà cung cấp";
+            // 
+            // thốngKêVàBáoCáoToolStripMenuItem
+            // 
+            thốngKêVàBáoCáoToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { thốngKêDoanhThuToolStripMenuItem, tínhLươngNhânViênToolStripMenuItem });
+            thốngKêVàBáoCáoToolStripMenuItem.Name = "thốngKêVàBáoCáoToolStripMenuItem";
+            thốngKêVàBáoCáoToolStripMenuItem.Size = new Size(161, 24);
+            thốngKêVàBáoCáoToolStripMenuItem.Text = "Thống kê và báo cáo";
+            // 
+            // thốngKêDoanhThuToolStripMenuItem
+            // 
+            thốngKêDoanhThuToolStripMenuItem.Name = "thốngKêDoanhThuToolStripMenuItem";
+            thốngKêDoanhThuToolStripMenuItem.Size = new Size(230, 26);
+            thốngKêDoanhThuToolStripMenuItem.Text = "Thống kê doanh thu";
+            // 
+            // tínhLươngNhânViênToolStripMenuItem
+            // 
+            tínhLươngNhânViênToolStripMenuItem.Name = "tínhLươngNhânViênToolStripMenuItem";
+            tínhLươngNhânViênToolStripMenuItem.Size = new Size(230, 26);
+            tínhLươngNhânViênToolStripMenuItem.Text = "Tính lương nhân viên";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            BackgroundImageLayout = ImageLayout.Stretch;
+            ClientSize = new Size(831, 569);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Form1";
+            WindowState = FormWindowState.Maximized;
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem tsmMenu;
+        private ToolStripMenuItem thôngTinNhânViênToolStripMenuItem;
+        private ToolStripMenuItem quảnLýChấmCôngToolStripMenuItem1;
+        private ToolStripMenuItem quảnLýHóaĐơnToolStripMenuItem;
+        private ToolStripMenuItem lậpHóaĐơnToolStripMenuItem;
+        private ToolStripMenuItem thôngTinHóaĐơnToolStripMenuItem;
+        private ToolStripMenuItem quảnLýKhoToolStripMenuItem;
+        private ToolStripMenuItem thôngTinKoToolStripMenuItem;
+        private ToolStripMenuItem thôngYinNhàToolStripMenuItem;
+        private ToolStripMenuItem thốngKêVàBáoCáoToolStripMenuItem;
+        private ToolStripMenuItem thốngKêDoanhThuToolStripMenuItem;
+        private ToolStripMenuItem tínhLươngNhânViênToolStripMenuItem;
+    }
+}
---
+++ QuanLySieuThiDienMay/Form1.cs
@@ -0,0 +1,10 @@
+namespace QuanLySieuThiDienMay
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
